017c66a7bd9ef77b3fd795298d02dc9a272937ee
Craig Wendel <wendelcraig@gmail.com>
2017-06-28 17:00:51 -0500

    Sequelize previous to do list to store tasks in a database rather than in memory for persistance.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const models = require('./models')
 
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
@@ -12,23 +13,48 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
 
 app.listen(3000, function () {
-  console.log('listening on to-do list')
+  console.log('listening on 3000')
 })
 
-const todos = []
-const completedTodos = []
-
 app.get('/', function (req, res) {
-  res.render('index', { todos: todos, completedTodos: completedTodos })
+  let incomplete = []
+  let complete = []
+
+  models.todos.findAll({
+  }).then(function (todos) {
+    for (var i = 0; i < todos.length; i++) {
+      if (todos[i].isCompleted === true) {
+        complete.push(todos[i])
+      } else {
+        incomplete.push(todos[i])
+      }
+    }
+  }).then(function () {
+    res.render('index', {
+      todos: incomplete,
+      completedTodos: complete
+    })
+  })
 })
 
 app.post('/', function (req, res) {
-  todos.push(req.body.task)
-  res.redirect('/')
+  let taskName = req.body.task
+  models.todos.create({
+    taskName: taskName,
+    isCompleted: false
+  }).then(function () {
+    res.redirect('/')
+  })
 })
-app.post('/complete', function (req,res){
-  const pop = req.body.task
-  todos.splice(todos.indexOf(pop), 1)
-  completedTodos.push(pop)
-  res.redirect('/')
+
+app.post('/complete', function (req, res) {
+  models.todos.update({
+    isCompleted: true
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function () {
+    res.redirect('/')
+  })
 })
